9f5a14cba9f43ef17aa104eb2db3f639df598ce3
Kyrvix <kyle.warrington@hotmail.com>
2019-03-15 11:57:02 +0000

    End of week 6

---
+++ K-workspace/Camel/src/main/java/com/qa/Camel/ContentBasedTesting.java
@@ -0,0 +1,40 @@
+package com.qa.Camel;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.component.properties.PropertiesComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.jms.ConnectionFactory;
+
+public class ContentBasedTesting {
+
+	public static void main(String args[]) throws Exception {
+		CamelContext context = new DefaultCamelContext();
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+
+		PropertiesComponent prop = context.getComponent("properties", PropertiesComponent.class);
+		prop.setLocation("classpath:system-properties.properties");
+
+		context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() {
+				from("ftp://test.rebex.net/pub/example?username=demo&password=password")
+				  .choice()
+				    .when(header("CamelFileName").endsWith(".png"))
+				      .to("activemq:queue:pngOrders")
+				    .when(header("CamelFileName").endsWith(".txt"))
+				      .to("activemq:queue:txtOrders")
+				    .otherwise()
+				      .to("activemq:queue:otherOrders");
+			}
+		});
+		context.start();
+		Thread.sleep(10000);
+		context.stop();
+	}
+
+}
---
+++ K-workspace/Camel/src/main/java/com/qa/Camel/Transforming.java
@@ -0,0 +1,33 @@
+package com.qa.Camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class Transforming {
+
+    public static void main(String args[]) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+//            public void configure() {
+//            	from("file:inbox?noop=true").transform(body().regexReplaceAll(" ", "<br/>")).to("file:outbox");
+//            }
+            public void configure() {
+            	from("file:inbox?noop=true").transform(new Expression() {
+            	  public <T> T evaluate(Exchange exchange, Class<T> type) {
+            		   String body = exchange.getIn().getBody(String.class);
+            		   body = body.replaceAll(" ","<br/>");
+            		   body = "<body>" + body + "</body>";
+            		   return (T) body;
+            	   }
+            	 }).to("file:outbox");
+            }
+        });
+        context.start();
+        Thread.sleep(10000); // need sleep to keep JVM running until the job is done
+        context.stop();
+    }
+
+}
